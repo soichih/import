050440bb0af41240a494fa5534870d7cd5186ac3
Soichi Hayashi <hayashis@iu.edu>
2017-09-28 00:54:42 +0000

    update nki importer to include task fMRI.

---
+++ clean/nki_files.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const mongo = require('mongodb');
+const async = require('async');
+const os = require('os');
+ 
+var url = 'mongodb://localhost:27017/warehouse';
+
+mongo.MongoClient.connect(url, function(err, db) {
+    if(err) throw err;
+    console.log("connected to mongo");
+
+    //find datasets that we need to clean up
+    var col = db.collection('datasets');
+    let project = new mongo.ObjectId("59a4d5a39823b1266ba14e13"); //nki-dev
+    let datatypes = [new mongo.ObjectId("58c33bcee13a50849b25879a"), new mongo.ObjectId("58c33c5fe13a50849b25879b")];
+    col.find({project, datatype: {$in: datatypes}}).toArray((err, datasets)=>{
+        if(err) throw err;
+        
+        async.eachSeries(datasets, (dataset, next_dataset)=>{
+            /*
+            var found = false;
+            exists.forEach(exist=>{
+                if(
+                    exist.meta.subject == dataset.meta.subject &&
+                    exist.meta.session == dataset.meta.session &&
+                    exist.datatype.toString() == dataset.datatype.toString() &&
+                    exist.datatype_tags.toString() == dataset.datatype_tags.toString()
+                ) found = true;
+            });
+            if(!found) new_datasets.push(dataset);
+            */
+            var s3_base = "data/Projects/RocklandSample/RawDataBIDS";
+            var files = [];
+            let base = s3_base+"/sub-"+dataset.meta.subject+"/ses-"+dataset.meta.session;
+
+            switch(dataset.datatype.toString()) {
+            case "58c33bcee13a50849b25879a": //t1
+                files.push({s3: base+"/anat/sub-"+dataset.meta.subject+"_ses-"+dataset.meta.session+"_T1w.nii.gz", local: "t1.nii.gz"});
+                break;
+            case "58c33c5fe13a50849b25879b": //dwi
+                files.push({s3: base+"/dwi/sub-"+dataset.meta.subject+"_ses-"+dataset.meta.session+"_dwi.nii.gz", local: "dwi.nii.gz"});
+                files.push({s3: base+"/dwi/sub-"+dataset.meta.subject+"_ses-"+dataset.meta.session+"_dwi.bval", local: "dwi.bval"});
+                files.push({s3: base+"/dwi/sub-"+dataset.meta.subject+"_ses-"+dataset.meta.session+"_dwi.bvec", local: "dwi.bvec"});
+                break;
+            default:
+                console.error("wrong datatype", dataset.datatype);
+                process.exit(1);
+            }
+
+            //update
+            dataset.storage_config = {files};
+            console.dir(dataset);
+            col.updateOne({_id: dataset._id}, {$set: {storage_config: {files}}}, (err, result)=>{
+                next_dataset();
+            });
+        }, err=>{
+            db.close();
+        });
+        /*
+        console.log("found ",new_datasets.length,"new dataset");
+        col.insertMany(new_datasets,function(err, result) {
+            if(err) throw err;
+            db.close();
+            cb();
+        });
+        */
+    });
+});
+
--- link_nki.js
+++ link_nki.js
@@ -2,54 +2,158 @@
 
 const fs = require('fs');
 const mkdirp = require('mkdirp');
+const async = require('async');
 
 nkidir="/mnt/fcp-indi/data/Projects/RocklandSample/RawDataBIDS";
 linkdir="/link/nki",
 
 //find sub-* directories
 fs.readdir(nkidir, (err, files)=>{
-    files.forEach(subject_dir=>{
-        if(!subject_dir.startsWith("sub-")) return;
+    async.eachSeries(files, (subject_dir, next_subject)=>{
+        if(!subject_dir.startsWith("sub-")) return next_subject();
 
         //list sessions
         fs.readdir(nkidir+"/"+subject_dir, (err, subfiles)=>{
-            subfiles.forEach(session_dir=>{
-                if(!session_dir.startsWith("ses-")) return;
+            if(err) return next_subject(err);
+            async.eachSeries(subfiles, (session_dir, next_session)=>{
+                if(!session_dir.startsWith("ses-")) return next_session();
 
                 var subject = subject_dir.substring(4);
                 var session = session_dir.substring(4);
                 var srcpath = nkidir+"/"+subject_dir+"/"+session_dir;
 
-                //find dwi
-                fs.stat(srcpath+"/dwi", (err, stats)=>{
-                    if(err) return;
-                    var dest = linkdir+"/"+subject+"/"+session+"/dwi";
-                    mkdirp(dest, err=>{
-                        fs.symlink(srcpath+"/dwi/"+subject_dir+"_"+session_dir+"_dwi.nii.gz", dest+"/dwi.nii.gz", err=>{
-                            console.log(subject,session,"dwi/dwi");         
+                async.series([
+                    next=>{
+                        //find dwi
+                        fs.stat(srcpath+"/dwi", (err, stats)=>{
+                            if(err) return next(); //no dwi. skip
+                            var dest = linkdir+"/"+subject+"/"+session+"/dwi";
+                            mkdirp(dest, err=>{
+                                if(err) return next(err);
+                                fs.symlink(srcpath+"/dwi/"+subject_dir+"_"+session_dir+"_dwi.nii.gz", dest+"/dwi.nii.gz", err=>{
+                                    console.log(subject,session,"dwi/dwi");         
+                                });
+                                fs.symlink(srcpath+"/dwi/"+subject_dir+"_"+session_dir+"_dwi.bvec", dest+"/dwi.bvecs", err=>{
+                                    console.log(subject,session,"dwi/bvecs");         
+                                });
+                                fs.symlink(srcpath+"/dwi/"+subject_dir+"_"+session_dir+"_dwi.bval", dest+"/dwi.bvals", err=>{
+                                    console.log(subject,session,"dwi/bvals");         
+                                });
+                                next();
+                            });
                         });
-                        fs.symlink(srcpath+"/dwi/"+subject_dir+"_"+session_dir+"_dwi.bvec", dest+"/dwi.bvecs", err=>{
-                            console.log(subject,session,"dwi/bvecs");         
+                    },
+
+                    next=>{
+                        //find anat
+                        fs.stat(srcpath+"/anat", (err, stats)=>{
+                            if(err) return next(); //no anat, skip
+                            var dest = linkdir+"/"+subject+"/"+session+"/anat";
+                            mkdirp(dest, err=>{
+                                if(err) return next(err);
+                                fs.symlink(srcpath+"/anat/"+subject_dir+"_"+session_dir+"_T1w.nii.gz", dest+"/t1.nii.gz", err=>{
+                                    //if(err) console.error(err);
+                                    console.log(subject,session,"anat");         
+                                    next();
+                                });
+                            });
                         });
-                        fs.symlink(srcpath+"/dwi/"+subject_dir+"_"+session_dir+"_dwi.bval", dest+"/dwi.bvals", err=>{
-                            console.log(subject,session,"dwi/bvals");         
+                    },
+
+                    next=>{
+                        //find func
+                        fs.stat(srcpath+"/func", (err, stats)=>{
+                            if(err) return next(); //no func . skip
+                            let dest = linkdir+"/"+subject+"/"+session+"/func";
+
+                            //find tasks
+                            fs.readdir(srcpath+"/func", (err, files)=>{
+                                if(err) return next(err);
+                                async.eachSeries(files, (file, next_file)=>{
+                                    if(!file.endsWith("bold.nii.gz")) return next_file();
+                                    let tokens = file.split("_");
+                                    let subject = tokens[0];
+                                    let session = tokens[1].substring(4);
+                                    let task = tokens[2].substring(5);
+                                    let acq = tokens[3].substring(4);
+                                    
+                                    let subdest = dest+"/"+task+"/"+acq;
+                                    mkdirp(subdest, err=>{
+                                        if(err) return next(err);
+                                        async.series([
+                                            n=>{
+                                                //all func has bold
+                                                fs.symlink(srcpath+"/func/"+file, subdest+"/bold.nii.gz", err=>{
+                                                    //console.log(subject,session,task,acq, "bold.nii.gz");
+                                                    n();
+                                                });
+                                            },
+
+                                            n=>{
+                                                //some has events.tsv
+                                                let tsvpath = srcpath+"/func/"+file.substring(0, file.length-11)+"events.tsv";
+                                                fs.stat(tsvpath, (err, stats)=>{
+                                                    if(err) return n(); //no evnets .. skip
+                                                    //if(err) return console.error(err);
+                                                    fs.symlink(tsvpath, subdest+"/events.tsv", err=>{
+                                                        console.log(tsvpath, subdest+"/events.tsv");
+                                                        n();
+                                                    });
+                                                });
+                                            },
+
+                                            n=>{
+                                                //some has sbref.tsv
+                                                let sbrefpath = srcpath+"/func/"+file.substring(0, file.length-11)+"sbref.nii.gz";
+                                                fs.stat(sbrefpath, (err, stats)=>{
+                                                    if(err) return n(); //no sbref skip
+                                                    //if(err) return console.error(err);
+                                                    fs.symlink(sbrefpath, subdest+"/sbref.nii.gz", err=>{
+                                                        console.log(sbrefpath, subdest+"/sbref.nii.gz");
+                                                        n();
+                                                    });
+                                                });
+                                            },
+
+                                            n=>{
+                                                //some has physio.tsv.gz
+                                                let physiopath = srcpath+"/func/"+file.substring(0, file.length-11)+"physio.tsv.gz";
+                                                fs.stat(physiopath, (err, stats)=>{
+                                                    if(err) return n(); //no evnets
+                                                    //if(err) return console.error(err);
+                                                    fs.symlink(physiopath, subdest+"/physio.tsv.gz", err=>{
+                                                        console.log(physiopath, subdest+"/physio.tsv.gz");
+                                                        n();
+                                                    });
+                                                });
+                                            },
+
+                                            n=>{
+                                                //some has physio.json
+                                                let physiojsonpath = srcpath+"/func/"+file.substring(0, file.length-11)+"physio.json";
+                                                fs.stat(physiojsonpath, (err, stats)=>{
+                                                    if(err) return n(); //no evnets
+                                                    fs.symlink(physiojsonpath, subdest+"/physio.json", err=>{
+                                                        console.log(physiojsonpath, subdest+"/physio.json");
+                                                        n();
+                                                    });
+                                                });
+                                            },
+                                        ], next_file);
+                                    });
+                                }, next);
+                            });
                         });
-                    });
-                });
+                    }, //next
                 
-                //find anat
-                fs.stat(srcpath+"/anat", (err, stats)=>{
-                    if(err) return;
-                    var dest = linkdir+"/"+subject+"/"+session+"/anat";
-                    mkdirp(dest, err=>{
-                        fs.symlink(srcpath+"/anat/"+subject_dir+"_"+session_dir+"_T1w.nii.gz", dest+"/t1.nii.gz", err=>{
-                            //if(err) console.error(err);
-                            console.log(subject,session,"anat");         
-                        });
-                    });
-                });
-            });
+                ], next_session);
+                
+
+            }, next_subject);
         });
+    }, err=>{
+        if(err) console.error(err);
+        console.log("done with all subject directories");
     });
 });
 
